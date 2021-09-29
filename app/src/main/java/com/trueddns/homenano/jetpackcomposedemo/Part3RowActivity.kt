55ca6c13abc9d240df42f0c1f6a508dede137dfc
Thongchai Subsaidee <chanoktrue@me.com>
2021-09-29 16:31:02 +0700

    finish Part3RowActivity

---
+++ app/src/main/java/com/trueddns/homenano/jetpackcomposedemo/Part3RowActivity.kt
@@ -0,0 +1,73 @@
+package com.trueddns.homenano.jetpackcomposedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+class Part3RowActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Home3()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Home3() {
+
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//    ){
+//        CustomItem(weight = 3f, MaterialTheme.colors.secondary)
+//        CustomItem(weight = 1f)
+//    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Start
+    ){
+        CustomItem(weight = 3f, MaterialTheme.colors.secondary)
+        CustomItem(weight = 1f)
+    }
+
+
+}
+
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .weight(weight),
+        color =  color
+
+    ) {}
+}
+
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight),
+        color =  color
+
+    ) {}
+}
