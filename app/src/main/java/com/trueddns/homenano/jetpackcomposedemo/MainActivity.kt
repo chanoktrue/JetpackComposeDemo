da672666973482afe2513a03569088705a4fabbf
Thongchai Subsaidee <chanoktrue@me.com>
2021-09-29 13:18:59 +0700

    init Part1StatesActivity

--- app/src/main/java/com/trueddns/homenano/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/trueddns/homenano/jetpackcomposedemo/MainActivity.kt
@@ -5,11 +5,19 @@ package com.trueddns.homenano.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.trueddns.homenano.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +39,47 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeDemoTheme {
         Greeting("Android")
     }
 }
+
+
+@Preview
+@Composable
+fun Home() {
+    val myText = "Test"
+    val myLists = listOf<String>("AAA", "BBB", "CCC", "DDD")
+    NamePicker(myText = myText, myLists = myLists)
+}
+
+@Composable
+fun NamePicker(myText: String, myLists: List<String>) {
+    Column {
+        Text(myText, style = TextStyle(fontSize = 30.sp))
+        
+        Divider()
+        
+        LazyColumn{
+            item(myLists.none()) {
+                myLists.forEach { name ->
+                    NamePickerItem(name = name)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NamePickerItem(name: String) {
+    Text(
+        name,
+        Modifier.clickable {
+            //todo
+        },
+        style = TextStyle(color = Color.Red)
+    )
+} 
---
+++ app/src/main/java/com/trueddns/homenano/jetpackcomposedemo/Part1StatesActivity.kt
@@ -0,0 +1,14 @@
+package com.trueddns.homenano.jetpackcomposedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class Part1StatesActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
